8a85a978f50b7469d6ce3811e2125f11cc6d93a9
Digital Nomad Dave <davidikin45@gmail.com>
2017-07-02 13:39:09 +0100

    Mongo Db

---
+++ DND.MongoPersistance/MongoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using Solution.Base.Implementation.Persistance.Mongo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND.MongoPersistance
+{
+    public class MongoApplicationDbContext : BaseMongoDBContext
+    {
+        public IMongoCollection<user> Users
+        {
+            get
+            {
+                return Database.GetCollection<user>("user");
+            }
+        }
+
+    }
+
+    public class user
+    {
+        public string name;
+    }
+}
---
+++ Solution.Base/Implementation/Persistance/Mongo/BaseMongoDbContext.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Base.Implementation.Persistance.Mongo
+{
+    public abstract class BaseMongoDBContext
+    {
+        public IMongoDatabase Database;
+
+        public BaseMongoDBContext()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultMongoConnectionString"].ConnectionString;
+            var databaseName = ConfigurationManager.AppSettings["DefaultMongoDatabaseName"];
+            var client = new MongoClient(connectionString);
+            Database = client.GetDatabase(databaseName);
+        }
+
+        public BsonDocument BuildInfo()
+        {
+            var buildInfoCommand = new BsonDocument("buildinfo", 1);
+            BsonDocument result = Database.RunCommand<BsonDocument>(buildInfoCommand);
+            return result;
+        }
+    }
+}
