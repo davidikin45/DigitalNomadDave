0b4d850967db64375c6de07c1d005cfc0f8ae257
Digital Nomad Dave <davidikin45@gmail.com>
2017-07-09 09:53:09 +0100

    Node SQL connection

--- DND.Node/server.js
+++ DND.Node/server.js
@@ -15,9 +15,52 @@ app.use(express.static(__dirname + "/public"));
 controllers.init(app);
 
 
+//4.SQL DB Connection config
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var config =
+    {
+        userName: "test",
+        password: "password12",
+        server: 'localhost', // update me
+        options:
+        {
+            database: 'DND', //update me
+            port: 1434,
+            rowCollectionOnRequestCompletion : true
+            , encrypt: true
+        }
+    };
+
 app.get("/api/users", function (req, res) {
     res.set("Content-Type", "text/plain");
     res.send({ name:"Shawn", isValid: true, group: "Admin"});
+});
+
+app.get("/api/sql", function (req, res) {
+    // Create connection to database
+    var sqlConnection = new Connection(config);
+    sqlConnection.on('connect', function (err) {
+        // If no error, then good to go...
+        if (err) {
+            console.log(err);
+        } else {
+            var request = new Request("SELECT * FROM BlogPost", function (err, rowCount, rows) {
+                var rowarray = [];
+                rows.forEach(function (columns) {
+                    var rowdata = new Object();
+                    columns.forEach(function (column) {
+                        rowdata[column.metadata.colName] = column.value;
+                    });
+                    rowarray.push(rowdata);
+                })
+                sqlConnection.close();
+                res.send(rowarray);
+            });
+            sqlConnection.execSql(request);
+        }       
+    }
+    );
 });
 
 var server = http.createServer(app);
